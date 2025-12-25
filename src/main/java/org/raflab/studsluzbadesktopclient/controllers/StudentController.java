d78f8115aaa85343e1204f866f307ea2cb03d508
Nikola Nedeljkovic <nikolanedeljkovic.official@gmail.com>
2025-12-26 00:40:16 +0100

    Revert "Added Uplate to student profile"
    This reverts commit c71ffd401cb6e449ec961d8c89f63132214daa3a.

--- src/main/java/org/raflab/studsluzbadesktopclient/controllers/StudentController.java
+++ src/main/java/org/raflab/studsluzbadesktopclient/controllers/StudentController.java
@@ -1,9 +1,6 @@
 package org.raflab.studsluzbadesktopclient.controllers;
 
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,7 +12,6 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.raflab.studsluzbacommon.dto.response.StudentIndeksResponseDTO;
 import org.raflab.studsluzbacommon.dto.response.StudentResponseDTO;
-import org.raflab.studsluzbacommon.dto.response.UplataResponse;
 import org.raflab.studsluzbadesktopclient.services.StudentIndexService;
 import org.raflab.studsluzbadesktopclient.utils.ErrorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,20 +22,16 @@ import java.io.IOException;
 
 @Component
 public class StudentController {
+    public TableView examsTable;
+    public ListView yearsList;
+    public TableView paymentsTable;
     @Autowired
     private ApplicationContext context;
 
     @Autowired
     private StudentIndexService studentIndexService;
-
-    public Label remainingRsdLabel;
-    public Label remainingEurLabel;
-
-    public TableView examsTable;
-    public ListView yearsList;
-    public TableView<UplataResponse> paymentsTable;
-
-    private ObservableList<UplataResponse> payments;
+//    @Autowired
+//    private StudentService studentService;
 
     public Label nameLabel;
     public Label avgLabel;
@@ -47,11 +39,6 @@ public class StudentController {
 
     private StudentIndeksResponseDTO studentIndex;
     private StudentResponseDTO student;
-
-    public void initialize(){
-        payments = FXCollections.observableArrayList();
-        paymentsTable.setItems(payments);
-    }
 
     public void setStudentIndex(StudentIndeksResponseDTO studentIndex){
         this.studentIndex = studentIndex;
@@ -71,23 +58,10 @@ public class StudentController {
         avgLabel.setText("Average: ");
         studentIndexService.findStudentAverageOcena(studentIndex.getId()).subscribe(ocena -> Platform.runLater(() -> avgLabel.setText("Average: " + ocena)), ErrorHandler::displayError);
     }
-    private void updatePayments(){
-        payments.clear();
-
-        studentIndexService.fetchPreostaliIznos(studentIndex.getId()).subscribe(amount -> Platform.runLater(() -> {
-            remainingRsdLabel.setText("RSD: " + String.format("%.2f", amount.getRsd()));
-            remainingEurLabel.setText("EUR: " + String.format("%.2f", amount.getEur()));
-        }), ErrorHandler::displayError);
-        studentIndexService.findStudentUplata(studentIndex.getId()).subscribe(payments::add, ErrorHandler::displayError);
-
-        paymentsTable.refresh();
-    }
-
     private void onStudentUpdate(){
         nameLabel.setText(student != null ? student.getIme() + " " + student.getPrezime() : "Student");
         espbLabel.setText("ESPB: " + (studentIndex != null ? studentIndex.getOstvarenoEspb() : 0));
         this.updateAverageOcena();
-        this.updatePayments();
     }
 
     @FXML
@@ -109,8 +83,5 @@ public class StudentController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public void handleCreateUplata(ActionEvent actionEvent) {
     }
 }
